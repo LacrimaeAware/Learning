74955dd65121a86fff6f2cd60639b5509e3766d8
LacrimaeAware <136732999+LacrimaeAware@users.noreply.github.com>
2024-06-04 17:25:54 -0400

    OutputProjStructure
    Random update

--- src/scripts/misc-helpers/output_project_structure.py
+++ src/scripts/misc-helpers/output_project_structure.py
@@ -3,8 +3,8 @@ import os
 def list_files(startpath):
     # Walk through each directory in the provided start path
     for root, dirs, files in os.walk(startpath):
-        # Exclude '.git' directory to avoid listing version control files
-        dirs[:] = [d for d in dirs if d != '.git']
+        # Exclude '.git' and '.idea' directory to avoid listing version control files
+        dirs[:] = [d for d in dirs if d != '.git' and d != '.idea']
 
         # Calculate how deep the directory is relative to startpath
         level = root.replace(startpath, '').count(os.sep)
@@ -24,7 +24,7 @@ def list_files(startpath):
 
 if __name__ == "__main__":
     # Navigate up two directories to the project root from the script location
-    project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 
     # Call function to list files starting from the calculated project path
     list_files(project_path)
